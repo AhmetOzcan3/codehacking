5e4785fd731108ba12906a9d935665c9f68c36f1
Ahmet <ozcan.ahmet@1ki3.com>
2017-04-11 18:50:53 +0300

    199.ders bitti.

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,50 @@
+@extends('admin.index')
+
+
+
+@section('content')
+
+
+	<h1>Create Users</h1>
+
+{!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store']) !!}
+
+
+
+	
+		<div class="form-group">
+	
+			{!! Form::label('name', 'İsim:') !!}
+			{!! Form::text('name', null, ['class'=>'form-control']) !!}
+		</div>
+
+			<div class="form-group">
+				
+			{!! Form::label('email', 'Mail Adresi:') !!}
+			{!! Form::email('email', null, ['class'=>'form-control']) !!}
+			
+			</div>
+
+					<div class="form-group">
+						
+					{!! Form::label('role_id', 'Rol:') !!}
+					{!! Form::select('role_id', [''=>'Seçenekler'], null, ['class'=>'form-control']) !!}
+					
+					</div>
+
+
+				<div class="form-group">
+
+				{!! Form::label('status', 'Durum:') !!}
+				{!! Form::select('status', array(1 => 'Çevrimiçi', 0=> 'Çevrimdışı'), null, ['class'=>'form-control']) !!}
+				
+				</div>
+	
+		<div class="form-group">
+	
+				{!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+		</div>
+		{!! Form::close() !!}	
+
+
+@stop
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,47 @@
+@extends('admin.index')
+
+
+
+@section('content')
+
+<h1>Users</h1>
+
+
+<table class="table">
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>İsim</th>
+        <th>E-mail</th>
+        <th>Rol</th>
+        <th>Durum</th>
+        <th>Oluşturuldu</th>
+        <th>Güncellendi</th>
+      </tr>
+    </thead>
+    <tbody>
+     
+     	@if($users)
+
+     	@foreach($users as $user)
+
+      <tr>
+        <td>{{$user->id}}</td>
+        <td>{{$user->name}}</td>
+        <td>{{$user->email}}</td>
+        <td>{{$user->role->name}}</td>
+        <td>{{$user->is_active == 1 ? 'Çevrimiçi' : 'Çevrimdışı' }}</td>
+        <td>{{$user->created_at->diffForHumans()}}</td>
+        <td>{{$user->updated_at->diffForHumans()}}</td>
+      </tr>
+
+      	@endforeach
+   
+   	@endif
+
+    </tbody>
+  </table>
+
+
+@stop
+
